c54507018fbc17d55d66e0d06f9a15656d349f1d
Rosan <rosan.syal@gmail.com>
2026-09-20 18:07:11 -0400

    created title screen

--- Scripts/usagi.gd
+++ Scripts/usagi.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var jump_sfx: AudioStreamPlayer2D = $jump_sfx
+
+
 const GRAVITY = 3000
 const JUMP_VELOCITY = -1500.0
 
@@ -15,6 +18,7 @@ func get_input(delta: float):
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sfx.play()
 	
 	move_and_slide()
 	
--- scripts/main.gd
+++ scripts/main.gd
@@ -10,9 +10,13 @@ const USAGI_START_POS := Vector2i(165, 461)
 const CAM_START_POS := Vector2i(576, 324)
 const START_SPEED: float = 10.0
 const MAX_SPEED: float = 25.0
+const SCORE_MOD: int = 10
+const SPEED_MOD: int = 5000
+
 var speed: float
 var screen_size: Vector2i
 var score: int
+var game_running: bool = false
 
 func _ready() -> void:
 	screen_size = get_window().size
@@ -26,17 +30,23 @@ func new_game():
 	ground.position = Vector2i(0, 0)
 
 func update_score():
-	hud.get_node("score").text = "Score: " + str(score / 10)
+	hud.get_node("score").text = "Score: " + str(score / SCORE_MOD)
 
 func _physics_process(delta: float) -> void:
-	speed = START_SPEED
-	score += speed
-	update_score()
-	
-	#move player and camera
-	usagi.position.x += speed
-	camera.position.x += speed
-	
-	#upate ground position
-	if camera.position.x - ground.position.x > screen_size.x * 1.5:
-		ground.position.x += screen_size.x
+	if game_running:
+		speed = START_SPEED + score / SPEED_MOD
+		print(speed)
+		score += speed
+		update_score()
+		
+		#move player and camera
+		usagi.position.x += speed
+		camera.position.x += speed
+		
+		#upate ground position
+		if camera.position.x - ground.position.x > screen_size.x * 1.5:
+			ground.position.x += screen_size.x
+	elif Input.is_action_just_pressed("jump"):
+		game_running = true
+		hud.get_node("start").hide()
+		
