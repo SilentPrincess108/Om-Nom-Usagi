6c2717a349ccff98643328c406b471079f898270
Rosan <rosan.syal@gmail.com>
2026-09-20 15:35:41 -0400

    parallax bg work

--- Scripts/usagi.gd
+++ Scripts/usagi.gd
@@ -13,7 +13,7 @@ func get_input(delta: float):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	move_and_slide()
---
+++ scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const USAGI_START_POS := Vector2i(165, 461)
+const CAM_START_POS := Vector2i(576, 324)
+const START_SPEED: float = 10.0
+var speed: float
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
