30d7ba56e078ef7e81ae44f1e20c07621917d797
Rosan <rosan.syal@gmail.com>
2026-09-20 17:06:50 -0400

    created working parallax bg, working on hud

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,42 @@
 extends Node
 
+@onready var usagi: CharacterBody2D = $Usagi
+@onready var camera: Camera2D = $Camera2D
+@onready var ground: StaticBody2D = $Ground
+@onready var hud: CanvasLayer = $HUD
+
+
 const USAGI_START_POS := Vector2i(165, 461)
 const CAM_START_POS := Vector2i(576, 324)
 const START_SPEED: float = 10.0
+const MAX_SPEED: float = 25.0
 var speed: float
+var screen_size: Vector2i
+var score: int
 
 func _ready() -> void:
-	pass
+	screen_size = get_window().size
+	new_game()
+
+func new_game():
+	score = 0
+	usagi.position = USAGI_START_POS
+	usagi.velocity = Vector2i(0, 0)
+	camera.position = CAM_START_POS
+	ground.position = Vector2i(0, 0)
+
+func update_score():
+	hud.get_node("score").text = "Score: " + str(score / 10)
 
 func _physics_process(delta: float) -> void:
-	pass
+	speed = START_SPEED
+	score += speed
+	update_score()
+	
+	#move player and camera
+	usagi.position.x += speed
+	camera.position.x += speed
+	
+	#upate ground position
+	if camera.position.x - ground.position.x > screen_size.x * 1.5:
+		ground.position.x += screen_size.x
